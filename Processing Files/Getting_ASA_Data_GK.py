844586b0486483bf696ae3d076c31d0cfee85221
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2025-05-21 11:03:53 -0400

    update

---
+++ Processing Files/Getting_ASA_Data_GK.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+import glob
+
+folder_path = 'ASA_Raw_Data_GK'
+
+dfs = []
+for file_path in glob.glob(os.path.join(folder_path, '*.csv')):
+    # Read the CSV file
+    df = pd.read_csv(file_path)
+    
+    if "uslc" in file_path.lower():
+        df['League'] = 'USLC'
+    elif "usl1" in file_path.lower():
+        df['League'] = 'USL1'
+    elif "mls" in file_path.lower():
+        df['League'] = 'MLS'
+    
+    
+    dfs.append(df)
+    
+asa_data = pd.concat(dfs, ignore_index=True)
+
+asa_data.drop(columns=['Season', 'Claiming', 'Handling', 'Shotstopping', 'Passing', 'Sweeping', 'Unnamed: 0'], inplace=True)
+
+asa_data['Goals Added p90'] = (asa_data['Goals Added']/asa_data['Minutes']) * 90
+
+
+
+asa_data.to_csv('asa_data_gk.csv', index=False)
