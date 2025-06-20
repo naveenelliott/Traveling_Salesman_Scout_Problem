ca9ef5b44c8970b2f317f85e88d1349ef03402d7
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2025-06-20 17:51:41 -0400

    update

--- PostProcessing Files/AddingLatLonBack_Drew.py
+++ PostProcessing Files/AddingLatLonBack_Drew.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-teset = pd.read_csv('Results/optimizer_MLS.csv')
+final_df = pd.read_csv('Results/final_uslc_sched.csv')
+del final_df['match_away_team'], final_df['team_score.y'], final_df['distance_km.y']
 
-final_df = pd.read_csv('Results/optimizer_MLS.csv')
-del final_df['id'], final_df['away_team'], final_df['next_away']
+final_df.rename(columns={'match_id': 'match',
+                         'corrected_next_team': 'next_home',
+                         'match_home_team': 'from_team'}, inplace=True)
 
 distance = pd.read_csv('address_lat_lon_ids.csv')
 del distance['team_id']
@@ -11,6 +13,8 @@ del distance['team_id']
 distance = distance.rename(columns={
     col: f"{col}_from" for col in distance.columns if col != 'Club'
 })
+
+distance['Club'] = distance['Club'].str.lower()
 
 
 final_df = pd.merge(
@@ -30,6 +34,8 @@ distance_next = distance.rename(columns={
     col: f"{col}_next" for col in distance.columns if col != 'Club'
 })
 
+distance_next['Club'] = distance_next['Club'].str.lower()
+
 final_df = pd.merge(
     final_df,
     distance_next,
@@ -39,8 +45,8 @@ final_df = pd.merge(
 del final_df['Club']
 
 final_df = final_df[['match', 'date', 'from_team', 'latitude_from', 'longitude_from',
-                     'next_date', 'next_home', 'latitude_next', 
-                     'longitude_next', 'distance', 'talent']]
+                     'next_home', 'latitude_next', 
+                     'longitude_next', 'avg_talent']]
 
 distance = pd.read_csv('distancesBetweenStadiums.csv')
 del distance['Team_1_ID'], distance['Team_2_ID']
@@ -51,12 +57,13 @@ final_df = pd.merge(final_df, distance, left_on=['from_team', 'next_home'], righ
 
 final_df['Distance_miles_x'] = final_df['Distance_miles_x'].fillna(final_df['Distance_miles_y'])
 
-final_df.drop(columns={'Team_1_x', 'Team_2_x', 'Team_1_y', 'Team_2_y', 'distance', 'Distance_miles_y'}, inplace=True)
+final_df.drop(columns={'Team_1_x', 'Team_2_x', 'Team_1_y', 'Team_2_y', 'Distance_miles_y'}, inplace=True)
 
 final_df.rename(columns={'Distance_miles_x': 'distance'}, inplace=True)
 
 final_df = final_df[['match', 'date', 'from_team', 'latitude_from', 'longitude_from',
-                     'next_date', 'next_home', 'latitude_next', 
-                     'longitude_next', 'distance', 'talent']]
+                     'next_home', 'latitude_next', 'longitude_next', 'distance', 'avg_talent']]
 
-final_df.to_csv('react_app_data_mls.csv', index=False)
+final_df['distance'] = final_df['distance'].fillna(0)
+
+final_df.to_csv('react_app_data_uslc.csv', index=False)
